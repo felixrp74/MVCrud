50ea5f5294a1ac8fed54b695f631e4332ce97efd
felixrp74 <felixrp74@gmail.com>
2021-09-30 21:37:30 -0500

    subir proyecto

---
+++ Controllers/TablaController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCrud.Models;
+using MVCrud.Models.ViewModels;
+
+
+namespace MVCrud.Controllers
+{
+    public class TablaController : Controller
+    {
+        // GET: Tabla
+        public ActionResult Index()
+        {
+            List<ListTablaViewModel> lista = new List<ListTablaViewModel>();
+
+            using ( CieloEntities db = new CieloEntities() )
+            {
+                lista = (from d in db.Persona
+                         select new ListTablaViewModel
+                         {
+                             Id = d.Id,
+                             Nombre = d.Nombre,
+                             Apellido = d.Apellido,
+                             FechaNacimiento = (DateTime)d.FechaNacimiento
+                         }).ToList();
+            }
+
+            return View(lista);  
+        }
+
+        public ActionResult Nuevo()
+        {
+            return View();
+        }  
+
+        [HttpPost]
+        public ActionResult Nuevo(TablaViewModel model)
+        {
+            try
+            {
+                if( ModelState.IsValid )
+                {
+                    using (CieloEntities db = new CieloEntities())
+                    {
+                        var oTabla = new Persona();
+
+                        oTabla.Nombre = model.Nombre;
+                        oTabla.Apellido = model.Apellido;
+                        oTabla.FechaNacimiento = DateTime.Now;
+
+                        db.Persona.Add(oTabla);
+                        db.SaveChanges();
+
+                    }
+                    return Redirect("~/Tabla/Index");
+                }
+                //return Redirect("~/Tabla/");
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+
+
+            }
+
+
+        }
+    }
+}
---
+++ Models/ViewModels/TablaViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCrud.Models.ViewModels
+{
+    public class TablaViewModel
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        [StringLength (50)]
+        [Display(Name = "Nombre")]
+        public string Nombre { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Apellido")]
+        public string Apellido{ get; set; }
+        
+        [Required]
+        [DataType(DataType.Date)]        
+        [Display(Name = "Fecha Nacimiento")]
+        public DateTime FechaNacimiento { get; set; }
+    }
+}
